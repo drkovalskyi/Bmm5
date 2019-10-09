196f4d5f1edba13fe4a1979c2cab474617ddf1d4
Dmytro Kovalskyi <dmytro@cern.ch>
2019-10-09 14:33:27 +0200

    do it properly

--- NanoAOD/python/nano_cff.py
+++ NanoAOD/python/nano_cff.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.nano_cff import *
+
 def nanoAOD_customizeBxToMuMu(process):
-    # process = nanoAOD_customizeCommon(process)
+    process.load('Bmm5.NanoAOD.BxToMuMu_cff')
     # Data 
-    process.nanoSequence   = cms.Sequence( process.nanoSequence + BxToMuMuSequence + BxToMuMuTables)
+    process.nanoSequence   = cms.Sequence( process.nanoSequence + process.BxToMuMuSequence + process.BxToMuMuTables)
     # MC
-    process.nanoSequenceMC = cms.Sequence( process.nanoSequenceMC + BxToMuMuMcSequence + BxToMuMuMcTables)
+    process.nanoSequenceMC = cms.Sequence( process.nanoSequenceMC + process.BxToMuMuMcSequence + process.BxToMuMuMcTables)
     return process
