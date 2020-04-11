1f7dc1f1eae0f2ba80be6c2ed5aee82d1c0e8319
Dmytro Kovalskyi <dmytro@cern.ch>
2020-04-11 18:39:59 +0200

    increased number of events per job

--- MCProduction/config/QCD_Pt-30toInf_BmmGenFilter_TuneCP5_13TeV_pythia8-RunIIFall18GS_cfg.py
+++ MCProduction/config/QCD_Pt-30toInf_BmmGenFilter_TuneCP5_13TeV_pythia8-RunIIFall18GS_cfg.py
@@ -26,7 +26,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(500000)
+    input = cms.untracked.int32(1000000)
 )
 
 # Input source
