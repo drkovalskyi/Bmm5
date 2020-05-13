6df2744a2e2229089113c884f06458460367df15
Dmytro Kovalskyi <dmytro@cern.ch>
2020-05-13 21:27:09 +0200

    split BxToMM and V0 producers

--- NanoAOD/python/nano_cff.py
+++ NanoAOD/python/nano_cff.py
@@ -4,9 +4,16 @@ from PhysicsTools.NanoAOD.nano_cff import *
 
 def nanoAOD_customizeBxToMuMu(process):
     process.load('Bmm5.NanoAOD.BxToMuMu_cff')
+    # Data 
+    process.nanoSequence   = cms.Sequence( process.nanoSequence + process.BxToMuMuSequence + process.BxToMuMuTables )
+    # MC
+    process.nanoSequenceMC = cms.Sequence( process.nanoSequenceMC + process.BxToMuMuMcSequence + process.BxToMuMuMcTables )
+    return process
+
+def nanoAOD_customizeV0ForMuonFake(process):
     process.load('Bmm5.NanoAOD.V0ForMuonFake_cff')
     # Data 
-    process.nanoSequence   = cms.Sequence( process.nanoSequence + process.BxToMuMuSequence + process.BxToMuMuTables + process.V0ForMuonFakeSequence + process.V0ForMuonFakeTables)
+    process.nanoSequence   = cms.Sequence( process.nanoSequence + process.V0ForMuonFakeSequence + process.V0ForMuonFakeTables)
     # MC
-    process.nanoSequenceMC = cms.Sequence( process.nanoSequenceMC + process.BxToMuMuMcSequence + process.BxToMuMuMcTables + process.V0ForMuonFakeMcSequence + process.V0ForMuonFakeMcTables)
+    process.nanoSequenceMC = cms.Sequence( process.nanoSequenceMC + process.V0ForMuonFakeMcSequence + process.V0ForMuonFakeMcTables)
     return process
