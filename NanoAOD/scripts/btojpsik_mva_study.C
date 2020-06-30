dc3def4c47f540b0f44c9026b86481ae85780445
Dmytro Kovalskyi <dmytro@cern.ch>
2020-06-30 11:38:05 +0200

    added multiple variables

--- NanoAOD/scripts/btojpsik_mva_study.C
+++ NanoAOD/scripts/btojpsik_mva_study.C
@@ -27,9 +27,25 @@ using namespace RooFit;
 
 std::string output_path = "/afs/cern.ch/user/d/dmytro/www/public_html/plots/bmm5_NanoAODv6-507/bjpsik-fit/";
 
+// name, branch name, number of bins, xmin, xmax
+// WARNING: signal purity must increase as x increases from xmin to xmax
+std::vector<std::tuple<std::string, std::string, unsigned int, float, float> > variables;
+void add_variables(){
+  variables.push_back(std::make_tuple("mva", "bkmm_bmm_mva", 100, 0, 1));
+  // variables.push_back(std::make_tuple("alpha", "(0.2-bkmm_jpsimc_alpha)", 100, 0, 0.2));
+  // variables.push_back(std::make_tuple("alphaXY", "bkmm_jpsimc_cosAlphaXY", 100, 0.999, 1));
+  // variables.push_back(std::make_tuple("spvip", "(10-bkmm_jpsimc_pvip/bkmm_jpsimc_pvipErr)", 100, 0, 10));
+  // variables.push_back(std::make_tuple("pvip", "(0.02-bkmm_jpsimc_pvip)", 100, 0, 0.02));
+  // variables.push_back(std::make_tuple("iso", "bkmm_bmm_iso", 100, 0, 1));
+  // variables.push_back(std::make_tuple("m1iso", "bkmm_bmm_m1iso", 100, 0, 1));
+  // variables.push_back(std::make_tuple("m2iso", "bkmm_bmm_m2iso", 100, 0, 1));
+  // variables.push_back(std::make_tuple("sl3d", "mm_kin_sl3d[bkmm_mm_index]", 100, 0, 100));
+  // variables.push_back(std::make_tuple("bdt", "bkmm_bmm_bdt", 50, -1, 1));
+}
+
 void print_canvas(std::string output_name_without_extention, 
 		  std::string path, 
-		  TCanvas* canvas){
+		  TVirtualPad* canvas){
   if (gSystem->AccessPathName(path.c_str())){
     gSystem->Exec(("mkdir -p " + path).c_str());
   }
@@ -53,8 +69,8 @@ void add_data(TFile* f){
   base_cut += "!TMath::IsNaN(bkmm_jpsimc_massErr)";
 
   TChain* mc_bkmm = new TChain("Events");
-  mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/0bb34079aa40de8c3fd5615d4816875f.root");
-  // mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/*.root");
+  // mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/0bb34079aa40de8c3fd5615d4816875f.root");
+  mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/*.root");
 
   TH1D* h_mc_bkmm_base = new TH1D("h_mc_bkmm_base", "", 50, 5.17, 5.45);
   mc_bkmm->Draw("bkmm_jpsimc_mass>>h_mc_bkmm_base", base_cut + "bkmm_gen_pdgId!=0", "goff");
@@ -64,13 +80,15 @@ void add_data(TFile* f){
   mc_bkmm->Draw("bkmm_jpsimc_mass>>h_mc_bkmm_kaon5", base_cut + "bkmm_gen_pdgId!=0&&bkmm_kaon_pt>5", "goff");
   h_mc_bkmm_kaon5->Write();
 
-  TH2D* h_mc_bkmm_mva = new TH2D("h_mc_bkmm_mva", "", 50, 5.17, 5.45, 50, 0.0, 1.0);
-  mc_bkmm->Draw("bkmm_bmm_mva:bkmm_jpsimc_mass>>h_mc_bkmm_mva", base_cut + "bkmm_gen_pdgId!=0", "goff");
-  h_mc_bkmm_mva->Write();
-
-  TH2D* h_mc_bkmm_bdt = new TH2D("h_mc_bkmm_bdt", "", 50, 5.17, 5.45, 50, -1.0, 1.0);
-  mc_bkmm->Draw("bkmm_bmm_bdt:bkmm_jpsimc_mass>>h_mc_bkmm_bdt", base_cut + "bkmm_gen_pdgId!=0", "goff");
-  h_mc_bkmm_bdt->Write();
+  for (auto var: variables){
+    std::string hist_name = "h_mc_bkmm_" + std::get<0>(var);
+    std::string draw_command = std::get<1>(var) + ":bkmm_jpsimc_mass>>h_mc_bkmm_" + std::get<0>(var);
+    TH2D* h_mc_bkmm = new TH2D(hist_name.c_str(), "", 
+			       50, 5.17, 5.45, 
+			       std::get<2>(var), std::get<3>(var), std::get<4>(var));
+    mc_bkmm->Draw(draw_command.c_str(), base_cut + "bkmm_gen_pdgId!=0", "goff");
+    h_mc_bkmm->Write();
+  }
 
   TH2D* h_mc_bkmm_mva_vs_npv = new TH2D("h_mc_bkmm_mva_vs_npv", "", 50, 0, 100, 50, 0.0, 1.0);
   mc_bkmm->Draw("bkmm_bmm_mva:PV_npvs>>h_mc_bkmm_mva_vs_npv", base_cut + "bkmm_gen_pdgId!=0", "goff");
@@ -92,13 +110,15 @@ void add_data(TFile* f){
   data->Draw("bkmm_jpsimc_mass>>h_data_kaon5", base_cut + "bkmm_kaon_pt>5", "goff");
   h_data_kaon5->Write();
 
-  TH2D* h_data_mva = new TH2D("h_data_mva", "", 50, 5.17, 5.45, 50, 0.0, 1.0);
-  data->Draw("bkmm_bmm_mva:bkmm_jpsimc_mass>>h_data_mva", base_cut, "goff");
-  h_data_mva->Write();
-
-  TH2D* h_data_bdt = new TH2D("h_data_bdt", "", 50, 5.17, 5.45, 50, -1.0, 1.0);
-  data->Draw("bkmm_bmm_bdt:bkmm_jpsimc_mass>>h_data_bdt", base_cut, "goff");
-  h_data_bdt->Write();
+  for (auto var: variables){
+    std::string hist_name = "h_data_" + std::get<0>(var);
+    std::string draw_command = std::get<1>(var) + ":bkmm_jpsimc_mass>>h_data_" + std::get<0>(var);
+    TH2D* h_data = new TH2D(hist_name.c_str(), "", 
+			    50, 5.17, 5.45, 
+			    std::get<2>(var), std::get<3>(var), std::get<4>(var));
+    data->Draw(draw_command.c_str(), base_cut, "goff");
+    h_data->Write();
+  }
 
   TH1D* h_data_npv = new TH1D("h_data_npv", "", 50, 0, 100);
   data->Draw("PV_npvs>>h_data_npv", base_cut, "goff");
@@ -114,9 +134,14 @@ struct Result {
   Result(const RooRealVar& nsig, const RooRealVar& nbkg):
     sig(nsig.getVal()), sig_err(nsig.getError()),
     bkg(nbkg.getVal()), bkg_err(nbkg.getError()){}
+  Result():
+    sig(0), sig_err(0),
+    bkg(0), bkg_err(0){}
 };
 
 Result fitHistogram(TH1* h_ref, TH1* h_test){
+  if (h_ref->Integral() < 1 or h_test->Integral() < 1) return Result();
+
   RooRealVar mass("mass", "BtoJpisK mass", 5.29);
   
   // signal pdf
@@ -130,11 +155,11 @@ Result fitHistogram(TH1* h_ref, TH1* h_test){
 
   // background pdf
   RooRealVar a0("a0", "a0", 0.0, -1., 1.) ;
-  RooRealVar a1("a1", "a1", 0.0, -1., 1.) ;
+  RooRealVar a1("a1", "a1", 0.0, -0.2, 0.2) ;
   // RooRealVar a2("a2", "a2", 0.0, -1., 1.) ;
   RooChebychev bkg("bkg", "Background", mass, RooArgSet(a0, a1));
-  RooRealVar Nsig("Nsig", "Nsig", 1000, 0, 1e9);
-  RooRealVar Nbkg("Nbkg", "Nbkg", 1, 0, 1e9);
+  RooRealVar Nsig("Nsig", "Nsig", h_test->Integral(), 0, h_test->Integral());
+  RooRealVar Nbkg("Nbkg", "Nbkg", 0, 0, h_test->Integral());
   RooAddPdf model("model", "", RooArgList(sig,bkg), RooArgList(Nsig,Nbkg));
 
   // test dataset
@@ -164,7 +189,55 @@ Result fitHistogram(TH1* h_ref, TH1* h_test){
   return Result(Nsig,Nbkg);
 }
 
+void process_variables(TFile* f){
+  for (auto var: variables){
+    unsigned int n = std::get<2>(var);
+    std::vector<Double_t> mc_eff;
+    std::vector<Double_t> data_eff;
+    std::vector<Double_t> data_over_mc_eff;
+    double data_total(-1);
+    double mc_total(-1);
+    // std::vector<Double_t> mc_eff_err;
+    // std::vector<Double_t> data_over_mc_eff_err;
+
+    for (unsigned int i=0; i <= n; ++i){
+      printf("processing %s %u\n",  std::get<0>(var).c_str(), i);
+      auto proj_mc = ((TH2*)f->Get(("h_mc_bkmm_" + std::get<0>(var)).c_str()))->ProjectionX("proj_mc", i, n+1);
+      auto proj_data = ((TH2*)f->Get(("h_data_" + std::get<0>(var)).c_str()))->ProjectionX("proj_data", i, n+1);
+      // if (i==0){
+      // 	proj_mc->Draw();
+      // 	print_canvas(std::get<0>(var) + "_h_mc_proj" + std::to_string(i), output_path, gPad);
+      // 	proj_data->Draw();
+      // 	print_canvas(std::get<0>(var) + "_h_data_proj" + std::to_string(i), output_path, gPad);
+      // }
+      auto result = fitHistogram(proj_mc, proj_data);
+      print_canvas(std::get<0>(var) + "_proj" + std::to_string(i), output_path, gPad);
+      if (i==0){
+	data_total = result.sig;
+	mc_total = proj_mc->Integral();
+      }
+      assert(data_total>0);
+      data_eff.push_back(result.sig/data_total);
+      assert(mc_total>0);
+      mc_eff.push_back(proj_mc->Integral()/mc_total);
+      data_over_mc_eff.push_back(mc_eff.back()>0?data_eff.back()/mc_eff.back():0);
+    }
+    auto gr = new TGraph(n, &mc_eff[0], &data_over_mc_eff[0]);
+    gr->SetMinimum(0);
+    gr->SetMaximum(1.5);
+    gr->GetXaxis()->SetLimits(0.,1.);
+    gr->GetXaxis()->SetTitle("MC efficiency");
+    gr->GetYaxis()->SetTitle("Data/MC efficiency ratio");
+    gr->SetTitle(std::get<0>(var).c_str());
+    gr->Draw("AP*");
+    print_canvas(std::get<0>(var) + "_eff", output_path, gPad);
+  }
+  
+}
+
 void btojpsik_mva_study(){
+  add_variables();
+
   const char* file_name = "btojpsik_mva_study.root";
   // Check if we already have histograms prepared
   if (gSystem->AccessPathName(file_name)){
@@ -180,6 +253,10 @@ void btojpsik_mva_study(){
 				 (TH1*)f->Get("h_data_base"));
 
   print_canvas("base", output_path, c1);
+  
+  process_variables(f);
+
+  return;
 
   unsigned int n = 50;
 
