ac6173c9b9344b321f135b544a73036e896720f8
Dmytro Kovalskyi <dmytro@cern.ch>
2020-07-01 15:22:06 +0200

    added Run2017 and Run2018 periods

--- NanoAOD/scripts/btojpsik_mva_study.C
+++ NanoAOD/scripts/btojpsik_mva_study.C
@@ -24,40 +24,89 @@
 #include <tuple>
 
 using namespace RooFit;
+using namespace std;
 
-std::string output_path = "/afs/cern.ch/user/d/dmytro/www/public_html/plots/bmm5_NanoAODv6-507/bjpsik-fit/";
+string output_path = "/afs/cern.ch/user/d/dmytro/www/public_html/plots/bmm5_NanoAODv6-507/bjpsik-fit/";
 
 // name, branch name, number of bins, xmin, xmax
 // WARNING: signal purity must increase as x increases from xmin to xmax
-std::vector<std::tuple<std::string, std::string, unsigned int, float, float> > variables;
-void add_variables(){
-  variables.push_back(std::make_tuple("mva", "bkmm_bmm_mva", 100, 0, 1));
-  // variables.push_back(std::make_tuple("alpha", "(0.2-bkmm_jpsimc_alpha)", 100, 0, 0.2));
-  // variables.push_back(std::make_tuple("alphaXY", "bkmm_jpsimc_cosAlphaXY", 100, 0.999, 1));
-  // variables.push_back(std::make_tuple("spvip", "(10-bkmm_jpsimc_pvip/bkmm_jpsimc_pvipErr)", 100, 0, 10));
-  // variables.push_back(std::make_tuple("pvip", "(0.02-bkmm_jpsimc_pvip)", 100, 0, 0.02));
-  // variables.push_back(std::make_tuple("iso", "bkmm_bmm_iso", 100, 0, 1));
-  // variables.push_back(std::make_tuple("m1iso", "bkmm_bmm_m1iso", 100, 0, 1));
-  // variables.push_back(std::make_tuple("m2iso", "bkmm_bmm_m2iso", 100, 0, 1));
-  // variables.push_back(std::make_tuple("sl3d", "mm_kin_sl3d[bkmm_mm_index]", 100, 0, 100));
-  // variables.push_back(std::make_tuple("bdt", "bkmm_bmm_bdt", 50, -1, 1));
+vector<tuple<string, string, unsigned int, float, float> > variables;
+void register_variables(){
+  variables.push_back(make_tuple("mva", "bkmm_bmm_mva", 100, 0, 1));
+  variables.push_back(make_tuple("alpha", "(0.2-bkmm_jpsimc_alpha)", 100, 0, 0.2));
+  variables.push_back(make_tuple("alphaXY", "bkmm_jpsimc_cosAlphaXY", 100, 0.999, 1));
+  variables.push_back(make_tuple("spvip", "(10-bkmm_jpsimc_pvip/bkmm_jpsimc_pvipErr)", 100, 0, 10));
+  variables.push_back(make_tuple("pvip", "(0.02-bkmm_jpsimc_pvip)", 100, 0, 0.02));
+  variables.push_back(make_tuple("iso", "bkmm_bmm_iso", 100, 0, 1));
+  variables.push_back(make_tuple("m1iso", "bkmm_bmm_m1iso", 100, 0, 1));
+  variables.push_back(make_tuple("m2iso", "bkmm_bmm_m2iso", 100, 0, 1));
+  variables.push_back(make_tuple("sl3d", "mm_kin_sl3d[bkmm_mm_index]", 100, 0, 100));
+  variables.push_back(make_tuple("bdt", "bkmm_bmm_bdt", 50, -1, 1));
 }
 
-void print_canvas(std::string output_name_without_extention, 
-		  std::string path, 
+// name, mc file(s), data file(s), trigger
+vector<tuple<string, string, string, string>> datasets;
+void register_data(){
+  string data_path = "/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/";
+
+  string mc_2018 = data_path + "BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/*.root";
+  datasets.push_back(make_tuple("Run2018A", 
+				mc_2018,
+				data_path + "Charmonium+Run2018A-17Sep2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_3_Jpsi"));
+  datasets.push_back(make_tuple("Run2018B", 
+				mc_2018,
+				data_path + "Charmonium+Run2018B-17Sep2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_3_Jpsi"));
+  datasets.push_back(make_tuple("Run2018C", 
+				mc_2018,
+				data_path + "Charmonium+Run2018C-17Sep2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_3_Jpsi"));
+  datasets.push_back(make_tuple("Run2018D", 
+  				// data_path + "BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/0bb34079aa40de8c3fd5615d4816875f.root",
+  				mc_2018,
+  				// data_path + "Charmonium+Run2018D-PromptReco-v2+MINIAOD/142c84fb8e8d03eea34e0cf3277a0656.root",
+  				data_path + "Charmonium+Run2018D-PromptReco-v2+MINIAOD/*.root",
+				"HLT_DoubleMu4_3_Jpsi"));
+
+  string mc_2017 = data_path + "BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v3+MINIAODSIM/*.root";
+  datasets.push_back(make_tuple("Run2017B", 
+				mc_2017,
+				data_path + "Charmonium+Run2017B-31Mar2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_Jpsi_NoVertexing"));
+  datasets.push_back(make_tuple("Run2017C", 
+				mc_2017,
+				data_path + "Charmonium+Run2017C-31Mar2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_Jpsi_NoVertexing"));
+  datasets.push_back(make_tuple("Run2017D", 
+				mc_2017,
+				data_path + "Charmonium+Run2017D-31Mar2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_Jpsi_NoVertexing"));
+  datasets.push_back(make_tuple("Run2017E", 
+				mc_2017,
+				data_path + "Charmonium+Run2017E-31Mar2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_Jpsi_NoVertexing"));
+  datasets.push_back(make_tuple("Run2017F", 
+				mc_2017,
+				data_path + "Charmonium+Run2017F-31Mar2018-v1+MINIAOD/*.root",
+				"HLT_DoubleMu4_Jpsi_NoVertexing"));
+}
+
+void print_canvas(string output_name_without_extention, 
+		  string path, 
 		  TVirtualPad* canvas){
   if (gSystem->AccessPathName(path.c_str())){
     gSystem->Exec(("mkdir -p " + path).c_str());
   }
-  std::string s = path + "/" + output_name_without_extention;
+  string s = path + "/" + output_name_without_extention;
   canvas->Print((s + ".png").c_str());
   canvas->Print((s + ".pdf").c_str());
   // canvas->Print((s + ".root").c_str());
 }
 
-void add_data(TFile* f){
+void add_data(TFile* f, TChain* mc_bkmm, TChain* data, const char* trigger){
 
-  TCut base_cut("HLT_DoubleMu4_3_Jpsi");
+  TCut base_cut(trigger);
   // TCut base_cut("HLT_DoubleMu4_Jpsi_NoVertexing");
   base_cut += "abs(Muon_eta[mm_mu1_index[bkmm_mm_index]])<1.4 && Muon_pt[mm_mu1_index[bkmm_mm_index]]>4";
   base_cut += "abs(Muon_eta[mm_mu2_index[bkmm_mm_index]])<1.4 && Muon_pt[mm_mu2_index[bkmm_mm_index]]>4";
@@ -68,10 +117,6 @@ void add_data(TFile* f){
   base_cut += "Muon_softMva[mm_mu1_index[bkmm_mm_index]]>0.45 && Muon_softMva[mm_mu2_index[bkmm_mm_index]]>0.45";
   base_cut += "!TMath::IsNaN(bkmm_jpsimc_massErr)";
 
-  TChain* mc_bkmm = new TChain("Events");
-  // mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/0bb34079aa40de8c3fd5615d4816875f.root");
-  mc_bkmm->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2+MINIAODSIM/*.root");
-
   TH1D* h_mc_bkmm_base = new TH1D("h_mc_bkmm_base", "", 50, 5.17, 5.45);
   mc_bkmm->Draw("bkmm_jpsimc_mass>>h_mc_bkmm_base", base_cut + "bkmm_gen_pdgId!=0", "goff");
   h_mc_bkmm_base->Write();
@@ -81,11 +126,11 @@ void add_data(TFile* f){
   h_mc_bkmm_kaon5->Write();
 
   for (auto var: variables){
-    std::string hist_name = "h_mc_bkmm_" + std::get<0>(var);
-    std::string draw_command = std::get<1>(var) + ":bkmm_jpsimc_mass>>h_mc_bkmm_" + std::get<0>(var);
+    string hist_name = "h_mc_bkmm_" + get<0>(var);
+    string draw_command = get<1>(var) + ":bkmm_jpsimc_mass>>h_mc_bkmm_" + get<0>(var);
     TH2D* h_mc_bkmm = new TH2D(hist_name.c_str(), "", 
 			       50, 5.17, 5.45, 
-			       std::get<2>(var), std::get<3>(var), std::get<4>(var));
+			       get<2>(var), get<3>(var), get<4>(var));
     mc_bkmm->Draw(draw_command.c_str(), base_cut + "bkmm_gen_pdgId!=0", "goff");
     h_mc_bkmm->Write();
   }
@@ -98,10 +143,6 @@ void add_data(TFile* f){
   mc_bkmm->Draw("bkmm_bmm_bdt:PV_npvs>>h_mc_bkmm_bdt_vs_npv", base_cut + "bkmm_gen_pdgId!=0", "goff");
   h_mc_bkmm_bdt_vs_npv->Write();
 
-  TChain* data = new TChain("Events");
-  data->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/Charmonium+Run2018D-PromptReco-v2+MINIAOD/*.root");
-  // data->Add("/eos/cms/store/group/phys_muon/dmytro/tmp/NanoAOD-skims/bkmm/507/Charmonium+Run2018D-PromptReco-v2+MINIAOD/142c84fb8e8d03eea34e0cf3277a0656.root");
-
   TH1D* h_data_base = new TH1D("h_data_base", "", 50, 5.17, 5.45);
   data->Draw("bkmm_jpsimc_mass>>h_data_base", base_cut, "goff");
   h_data_base->Write();
@@ -111,11 +152,11 @@ void add_data(TFile* f){
   h_data_kaon5->Write();
 
   for (auto var: variables){
-    std::string hist_name = "h_data_" + std::get<0>(var);
-    std::string draw_command = std::get<1>(var) + ":bkmm_jpsimc_mass>>h_data_" + std::get<0>(var);
+    string hist_name = "h_data_" + get<0>(var);
+    string draw_command = get<1>(var) + ":bkmm_jpsimc_mass>>h_data_" + get<0>(var);
     TH2D* h_data = new TH2D(hist_name.c_str(), "", 
 			    50, 5.17, 5.45, 
-			    std::get<2>(var), std::get<3>(var), std::get<4>(var));
+			    get<2>(var), get<3>(var), get<4>(var));
     data->Draw(draw_command.c_str(), base_cut, "goff");
     h_data->Write();
   }
@@ -189,29 +230,29 @@ Result fitHistogram(TH1* h_ref, TH1* h_test){
   return Result(Nsig,Nbkg);
 }
 
-void process_variables(TFile* f){
+void process_variables(TFile* f, string prefix){
   for (auto var: variables){
-    unsigned int n = std::get<2>(var);
-    std::vector<Double_t> mc_eff;
-    std::vector<Double_t> data_eff;
-    std::vector<Double_t> data_over_mc_eff;
+    unsigned int n = get<2>(var);
+    vector<Double_t> mc_eff;
+    vector<Double_t> data_eff;
+    vector<Double_t> data_over_mc_eff;
     double data_total(-1);
     double mc_total(-1);
-    // std::vector<Double_t> mc_eff_err;
-    // std::vector<Double_t> data_over_mc_eff_err;
+    // vector<Double_t> mc_eff_err;
+    // vector<Double_t> data_over_mc_eff_err;
 
     for (unsigned int i=0; i <= n; ++i){
-      printf("processing %s %u\n",  std::get<0>(var).c_str(), i);
-      auto proj_mc = ((TH2*)f->Get(("h_mc_bkmm_" + std::get<0>(var)).c_str()))->ProjectionX("proj_mc", i, n+1);
-      auto proj_data = ((TH2*)f->Get(("h_data_" + std::get<0>(var)).c_str()))->ProjectionX("proj_data", i, n+1);
+      printf("processing %s %u\n",  get<0>(var).c_str(), i);
+      auto proj_mc = ((TH2*)f->Get(("h_mc_bkmm_" + get<0>(var)).c_str()))->ProjectionX("proj_mc", i, n+1);
+      auto proj_data = ((TH2*)f->Get(("h_data_" + get<0>(var)).c_str()))->ProjectionX("proj_data", i, n+1);
       // if (i==0){
       // 	proj_mc->Draw();
-      // 	print_canvas(std::get<0>(var) + "_h_mc_proj" + std::to_string(i), output_path, gPad);
+      // 	print_canvas(prefix + "-" + get<0>(var) + "_h_mc_proj" + to_string(i), output_path, gPad);
       // 	proj_data->Draw();
-      // 	print_canvas(std::get<0>(var) + "_h_data_proj" + std::to_string(i), output_path, gPad);
+      // 	print_canvas(prefix + "-" + get<0>(var) + "_h_data_proj" + to_string(i), output_path, gPad);
       // }
       auto result = fitHistogram(proj_mc, proj_data);
-      print_canvas(std::get<0>(var) + "_proj" + std::to_string(i), output_path, gPad);
+      print_canvas(prefix + "-" + get<0>(var) + "_proj" + to_string(i), output_path, gPad);
       if (i==0){
 	data_total = result.sig;
 	mc_total = proj_mc->Integral();
@@ -228,102 +269,122 @@ void process_variables(TFile* f){
     gr->GetXaxis()->SetLimits(0.,1.);
     gr->GetXaxis()->SetTitle("MC efficiency");
     gr->GetYaxis()->SetTitle("Data/MC efficiency ratio");
-    gr->SetTitle(std::get<0>(var).c_str());
+    gr->SetTitle(get<0>(var).c_str());
     gr->Draw("AP*");
-    print_canvas(std::get<0>(var) + "_eff", output_path, gPad);
+    print_canvas(prefix + "-" + get<0>(var) + "_eff", output_path, gPad);
   }
   
-}
-
-void btojpsik_mva_study(){
-  add_variables();
-
-  const char* file_name = "btojpsik_mva_study.root";
-  // Check if we already have histograms prepared
-  if (gSystem->AccessPathName(file_name)){
-    printf("Recreating histograms\n");
-    TFile* f = TFile::Open(file_name,"RECREATE");
-    add_data(f);    
-    f->Close();
-  }
-  TFile* f = TFile::Open(file_name);
-  
-  TCanvas* c1 = new TCanvas("c1", "c1", 600, 600);
-  auto result_all = fitHistogram((TH1*)f->Get("h_mc_bkmm_base"), 
-				 (TH1*)f->Get("h_data_base"));
-
-  print_canvas("base", output_path, c1);
-  
-  process_variables(f);
-
-  return;
-
-  unsigned int n = 50;
-
-  // MVA
-  std::vector<std::tuple<Result, unsigned int>> results_mva;
-
-  for (unsigned int i=1; i <= n; ++i){
-    auto proj_mc = ((TH2*)f->Get("h_mc_bkmm_mva"))->ProjectionX("proj_mc", i);
-    auto proj_data = ((TH2*)f->Get("h_data_mva"))->ProjectionX("proj_data", i);
-    auto result = fitHistogram(proj_mc, proj_data);
-    results_mva.push_back(std::make_tuple(result, proj_mc->Integral()));
-    print_canvas("mva_proj"+std::to_string(i), output_path, c1);
-  }
-
-  // BDT
-  std::vector<std::tuple<Result, unsigned int>> results_bdt;
-
-  for (unsigned int i=1; i <= n; ++i){
-    auto proj_mc = ((TH2*)f->Get("h_mc_bkmm_bdt"))->ProjectionX("proj_mc", i);
-    auto proj_data = ((TH2*)f->Get("h_data_bdt"))->ProjectionX("proj_data", i);
-    auto result = fitHistogram(proj_mc, proj_data);
-    results_bdt.push_back(std::make_tuple(result, proj_mc->Integral()));
-    print_canvas("bdt_proj"+std::to_string(i), output_path, c1);
-  }
-
-
-  // Pileup treatment
   TH1* h_mc_npv = ((TH2*)f->Get("h_mc_bkmm_mva_vs_npv"))->ProjectionX();
   TH1* h_data_npv = ((TH1*)f->Get("h_data_npv"));
   h_mc_npv->SetLineWidth(2);
   h_mc_npv->SetLineColor(kBlue);
   h_mc_npv->Draw();
-  print_canvas("mc_npv", output_path, c1);
+  print_canvas(prefix + "-" + "mc_npv", output_path, gPad);
 
   h_data_npv->SetLineWidth(2);
   h_data_npv->SetLineColor(kBlue);
   h_data_npv->Draw();
-  print_canvas("data_npv", output_path, c1);
+  print_canvas(prefix + "-" + "data_npv", output_path, gPad);
+}
+
+void btojpsik_mva_study(){
+  register_variables();
+  register_data();
+
+  for (auto dataset: datasets){
+    string file_name = "btojpsik_mva_study-" + get<0>(dataset) + ".root";
+    // Check if we already have histograms prepared
+    if (gSystem->AccessPathName(file_name.c_str())){
+      printf("Recreating histograms for %s\n", get<0>(dataset).c_str());
+      TFile* f = TFile::Open(file_name.c_str(), "RECREATE");
+      TChain* mc = new TChain("Events");
+      mc->Add(get<1>(dataset).c_str());
+      TChain* data = new TChain("Events");
+      data->Add(get<2>(dataset).c_str());
+      add_data(f, mc, data, (get<3>(dataset).c_str()));    
+      f->Close();
+    }
+    // continue;
+
+    TFile* f = TFile::Open(file_name.c_str());
   
-  // Results
-  printf("Total: %0.1f+/-%0.1f\n", result_all.sig, result_all.sig_err);
-  printf("MVA:\n");
-  for (unsigned int i=0; i < n; ++i){
-    double eff_data = std::get<0>(results_mva[i]).sig/std::get<0>(results_mva[0]).sig;
-    double eff_mc = std::get<1>(results_mva[i])/float(std::get<1>(results_mva[0]));
-    printf("\t[%u] \tmva_data: %0.1f+/-%0.1f \tmva_mc:%u \teff_data: %0.1f%% \teff_mc: %0.1f%% \teff_mc/eff_data: %0.2f\n", 
-	   i+1,
-	   std::get<0>(results_mva[i]).sig, 
-	   std::get<0>(results_mva[i]).sig_err, 
-	   std::get<1>(results_mva[i]),
-	   eff_data*100.,
-	   eff_mc*100.,
-	   eff_data>0?eff_mc/eff_data:0
-	   ); 
+    TCanvas* c1 = new TCanvas("c1", "c1", 600, 600);
+    auto result_all = fitHistogram((TH1*)f->Get("h_mc_bkmm_base"), 
+				   (TH1*)f->Get("h_data_base"));
+    
+    print_canvas(get<0>(dataset) + "base", output_path, c1);
+      
+    process_variables(f, get<0>(dataset));
   }
-  printf("BDT:\n");
-  for (unsigned int i=0; i < n; ++i){
-    double eff_data = std::get<0>(results_bdt[i]).sig/std::get<0>(results_bdt[0]).sig;
-    double eff_mc = std::get<1>(results_bdt[i])/float(std::get<1>(results_bdt[0]));
-    printf("\t[%u] \tbdt_data: %0.1f+/-%0.1f \tbdt_mc:%u \teff_data: %0.1f%% \teff_mc: %0.1f%% \teff_mc/eff_data: %0.2f\n", 
-	   i+1,
-	   std::get<0>(results_bdt[i]).sig, 
-	   std::get<0>(results_bdt[i]).sig_err, 
-	   std::get<1>(results_bdt[i]),
-	   eff_data*100.,
-	   eff_mc*100.,
-	   eff_data>0?eff_mc/eff_data:0
-	   ); 
-  }
+
+  return;
+
+  // unsigned int n = 50;
+
+  // // MVA
+  // vector<tuple<Result, unsigned int>> results_mva;
+
+  // for (unsigned int i=1; i <= n; ++i){
+  //   auto proj_mc = ((TH2*)f->Get("h_mc_bkmm_mva"))->ProjectionX("proj_mc", i);
+  //   auto proj_data = ((TH2*)f->Get("h_data_mva"))->ProjectionX("proj_data", i);
+  //   auto result = fitHistogram(proj_mc, proj_data);
+  //   results_mva.push_back(make_tuple(result, proj_mc->Integral()));
+  //   print_canvas("mva_proj"+to_string(i), output_path, c1);
+  // }
+
+  // // BDT
+  // vector<tuple<Result, unsigned int>> results_bdt;
+
+  // for (unsigned int i=1; i <= n; ++i){
+  //   auto proj_mc = ((TH2*)f->Get("h_mc_bkmm_bdt"))->ProjectionX("proj_mc", i);
+  //   auto proj_data = ((TH2*)f->Get("h_data_bdt"))->ProjectionX("proj_data", i);
+  //   auto result = fitHistogram(proj_mc, proj_data);
+  //   results_bdt.push_back(make_tuple(result, proj_mc->Integral()));
+  //   print_canvas("bdt_proj"+to_string(i), output_path, c1);
+  // }
+
+
+  // // Pileup treatment
+  // TH1* h_mc_npv = ((TH2*)f->Get("h_mc_bkmm_mva_vs_npv"))->ProjectionX();
+  // TH1* h_data_npv = ((TH1*)f->Get("h_data_npv"));
+  // h_mc_npv->SetLineWidth(2);
+  // h_mc_npv->SetLineColor(kBlue);
+  // h_mc_npv->Draw();
+  // print_canvas("mc_npv", output_path, c1);
+
+  // h_data_npv->SetLineWidth(2);
+  // h_data_npv->SetLineColor(kBlue);
+  // h_data_npv->Draw();
+  // print_canvas("data_npv", output_path, c1);
+  
+  // // Results
+  // printf("Total: %0.1f+/-%0.1f\n", result_all.sig, result_all.sig_err);
+  // printf("MVA:\n");
+  // for (unsigned int i=0; i < n; ++i){
+  //   double eff_data = get<0>(results_mva[i]).sig/get<0>(results_mva[0]).sig;
+  //   double eff_mc = get<1>(results_mva[i])/float(get<1>(results_mva[0]));
+  //   printf("\t[%u] \tmva_data: %0.1f+/-%0.1f \tmva_mc:%u \teff_data: %0.1f%% \teff_mc: %0.1f%% \teff_mc/eff_data: %0.2f\n", 
+  // 	   i+1,
+  // 	   get<0>(results_mva[i]).sig, 
+  // 	   get<0>(results_mva[i]).sig_err, 
+  // 	   get<1>(results_mva[i]),
+  // 	   eff_data*100.,
+  // 	   eff_mc*100.,
+  // 	   eff_data>0?eff_mc/eff_data:0
+  // 	   ); 
+  // }
+  // printf("BDT:\n");
+  // for (unsigned int i=0; i < n; ++i){
+  //   double eff_data = get<0>(results_bdt[i]).sig/get<0>(results_bdt[0]).sig;
+  //   double eff_mc = get<1>(results_bdt[i])/float(get<1>(results_bdt[0]));
+  //   printf("\t[%u] \tbdt_data: %0.1f+/-%0.1f \tbdt_mc:%u \teff_data: %0.1f%% \teff_mc: %0.1f%% \teff_mc/eff_data: %0.2f\n", 
+  // 	   i+1,
+  // 	   get<0>(results_bdt[i]).sig, 
+  // 	   get<0>(results_bdt[i]).sig_err, 
+  // 	   get<1>(results_bdt[i]),
+  // 	   eff_data*100.,
+  // 	   eff_mc*100.,
+  // 	   eff_data>0?eff_mc/eff_data:0
+  // 	   ); 
+  // }
 }
