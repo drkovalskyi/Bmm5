ce2c99e182e055f7c0268a29464b33cab67590de
Dmytro Kovalskyi <dmytro@cern.ch>
2020-10-22 19:12:25 +0200

    tuned

--- NanoAOD/test/skim_fakes.py
+++ NanoAOD/test/skim_fakes.py
@@ -7,26 +7,31 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 
-dataset = "/LambdaBToPPi_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM"
+dataset = None
+# dataset = "/LambdaBToPPi_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM"
 
 # Input source
 process.source = cms.Source(
     "PoolSource",
-    fileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring(
+        '/store/user/dmytro/tmp/store+mc+RunIIAutumn18MiniAOD+QCD_Pt-30to50_MuEnrichedPt5_TuneCP5_13TeV_pythia8+MINIAODSIM+102X_upgrade2018_realistic_v15-v3+60000+4D8AFF1E-98C3-464B-99F6-370070C6405D.root'
+    )
 )
 
-files = subprocess.check_output('dasgoclient -query "file dataset=%s"' % dataset, shell=True)
-for f in files.split('\n'):
-    if re.search('\S',f):
-        process.source.fileNames.append(f)
+output_name = "output.root"
+if dataset:
+    files = subprocess.check_output('dasgoclient -query "file dataset=%s"' % dataset, shell=True)
+    for f in files.split('\n'):
+        if re.search('\S',f):
+            process.source.fileNames.append(f)
 
-output_name = re.sub('\/','+', re.sub('^\/', '', dataset))
+    output_name = re.sub('\/','+', re.sub('^\/', '', dataset)) + ".root"
 
 process.outputPath = cms.EndPath()
 
 process.output = cms.OutputModule(
     "PoolOutputModule",
-    fileName = cms.untracked.string('file:%s.root' % output_name),
+    fileName = cms.untracked.string(output_name),
     SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('filterPath'))                              
 )
 
