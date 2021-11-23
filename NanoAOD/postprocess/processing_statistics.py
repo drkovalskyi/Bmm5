d5802a5f364430a1817a421e0d890bd65927deeb
Dmytro Kovalskyi <dmytro@cern.ch>
2021-11-23 15:17:51 +0100

    simple tool to check sample consistency

---
+++ NanoAOD/postprocess/processing_statistics.py
@@ -0,0 +1,35 @@
+import ROOT, copy, subprocess, re
+
+path = "/eos/cms/store/group/phys_bphys/bmm/bmm5/PostProcessing/FlatNtuples/517"
+
+datasets = {}
+
+command = 'find -L %s -maxdepth 2 -type d' % (path)
+all_folders = subprocess.check_output(command, shell=True).splitlines()
+
+pattern = 'Charmonium'
+
+for folder in all_folders:
+    if pattern != "":
+        if not re.search(pattern, folder):
+            continue
+    match = re.search("([^\/]+)\/([^\/]+)\/*$", folder)
+    if match:
+        type = match.group(1)
+        ds = match.group(2)
+
+        if not ds in datasets:
+            datasets[ds] = []
+        datasets[ds].append(type) 
+
+for ds in sorted(datasets):
+    print ds
+    for type in sorted(datasets[ds]):
+        print "\t%-30s " % type,
+        chain = ROOT.TChain("info")
+        chain.Add("%s/%s/%s/*.root" % (path, type, ds))
+        n_processed = 0
+        for entry in chain:
+            n_processed += entry.n_processed
+        print " \t:", n_processed
+    
