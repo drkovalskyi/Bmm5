e04df35a774c0af6f457bfe134dc5db36ce61489
Dmytro Kovalskyi <dmytro@cern.ch>
2022-01-20 17:29:38 +0100

    modified to Bs fit

--- NanoAOD/validation/dimuon_fit.C
+++ NanoAOD/validation/dimuon_fit.C
@@ -34,26 +34,26 @@
 using namespace RooFit;
 using namespace std;
 
-string output_path = "/afs/cern.ch/user/d/dmytro/www/public_html/plots/bmm5_NanoAODv8-517/dimuon_fits/";
+string output_path = "/afs/cern.ch/user/d/dmytro/www/public_html/plots/bmm5_NanoAODv8-517/dimuon-Bs/";
 
 const bool remake_input_workspaces = false;
 const bool pre_processing_only = false; // Just produce samples 
 const bool fit_samples = false; // no effect on results, just extra info on plots
 
-const double mm_mass_min = 3.00;
-const double mm_mass_max = 3.16;
+const double mm_mass_min = 5.2;
+const double mm_mass_max = 5.6;
 const double mm_mass_err_min = 0.005;
 const double mm_mass_err_max = 0.100;
 
 // Running options
 const bool silent_roofit = true;
 
-string path = "/eos/cms/store/group/phys_bphys/bmm/bmm5/PostProcessing/FlatNtuples/517/dimuon/";
+string path = "/eos/cms/store/group/phys_bphys/bmm/bmm5/PostProcessing/FlatNtuples/517/fit/";
 
 
 vector<string> files =
   {
-    path + "BuToJpsiK_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v2+MINIAODSIM/1030c1af75518bcb94c564e542884df3.root"
+    path + "BsToMuMu_BMuonFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen+RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v1+MINIAODSIM/391cf45093fbe4ff6ab26a77246c11e2.root"
   };
 
     
@@ -71,28 +71,36 @@ void print_canvas(string output_name_without_extention,
 
 
 void dimuon_fit(){
+  TCanvas* c1 = new TCanvas("c1", "c1", 800, 600);
+
+  RooRealVar bdt("bdt", "bdt", 0);
   RooRealVar mass("m", "mass", mm_mass_min, mm_mass_max);
   RooRealVar mass_err("me", "mass_err", mm_mass_err_min, mm_mass_err_max);
 
-  TChain* chain = new TChain("jpsiMC");
+  TChain* chain = new TChain("bsmmMc");
   for (auto f: files)
     chain->Add(f.c_str());
   Long64_t n = chain->GetEntries();
   printf("Total number of events: %lld\n", n);
   
-  RooDataSet data("data", "data", RooArgSet(mass, mass_err), Import(*chain));
-  printf("RooDataSet storage type: %u\n", RooAbsData::getDefaultStorageType());
+  // RooDataSet data("data", "data", RooArgSet(mass, mass_err), Import(*chain), Cut("int(me*10000)-125*int(me*80)==0"));
+  RooDataSet data("data", "data", RooArgSet(mass, mass_err, bdt), Import(*chain), Cut("bdt>0.995 && int(me*10000)-100*int(me*100)==0"));
+  data.Print("V");
+  // return;
   
   // Build pdf to be used as a conditinal pdf (mass|mass_err)
   RooRealVar mean("mean", "mean", (mm_mass_max + mm_mass_min)/2, mm_mass_min, mm_mass_max);
   RooRealVar s("s", "resolution", 1, 0, 20);
-  // s.setConstant(true);
   RooFormulaVar sigma("sigma", "", "@0*@1", RooArgList(s, mass_err));
   RooRealVar cb_alpha("cb_alpha", "Crystal-Ball gaussian cutoff", 1, 0.1, 10);
   RooRealVar cb_n("cb_n", "Crystal-Ball exponent of the power-low tail", 5, 0, 10000);
   RooCBShape cond_pdf("cond_pdf", "Conditional PDF", mass, mean, sigma, cb_alpha, cb_n);
 
   // Fit signal model to extract parameters
+  cond_pdf.fitTo(data, ConditionalObservables(mass_err), NumCPU(16), Timer(true));
+  cb_alpha.setConstant(true);
+  cb_n.setConstant(true);
+  s.setConstant(true);
   cond_pdf.fitTo(data, ConditionalObservables(mass_err), NumCPU(16), Timer(true));
     
   // Plot results of the fit
@@ -102,6 +110,8 @@ void dimuon_fit(){
   cond_pdf.plotOn(frame, ProjWData(mass_err, data));
   // pdf_bsmm.plotOn(frame_bsmm, ProjWData(*mass_err, *data_bsmm));
   // pdf_bsmm.plotOn(frame_bsmm, ProjWData(*mass_err, *data_bsmm), NumCPU(16), Normalization(data_bsmm->sumEntries(), RooAbsReal::NumEvent));
+  cond_pdf.paramOn(frame, Layout(0.55, 0.85, 0.85));
+  frame->getAttText()->SetTextSize(0.03);
   frame->Draw();
   print_canvas("cond_pdf", output_path, gPad);
 }
