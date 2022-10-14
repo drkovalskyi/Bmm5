774467da836e9ceafbfbe6487c35fd52597e0c61
Dmytro Kovalskyi <dmytro@cern.ch>
2022-10-14 10:33:16 +0200

    removed old customization, which no longer works

--- NanoAOD/python/nano_cff.py
+++ NanoAOD/python/nano_cff.py
@@ -2,17 +2,6 @@ from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.nano_cff import *
 
-def nanoAOD_customizeBxToMuMu(process):
-    process.load('Bmm5.NanoAOD.BxToMuMu_cff')
-    process.load('Bmm5.NanoAOD.UpdateSlimmedMuons_cff')
-    process.load('PhysicsTools.NanoAOD.muons_cff')
-    # Data 
-    process.nanoSequence   = cms.Sequence(process.slimmedMuons + process.nanoSequence + process.BxToMuMuSequence + process.BxToMuMuTables)
-    # MC
-    process.nanoSequenceMC = cms.Sequence(process.slimmedMuons + process.nanoSequenceMC + process.BxToMuMuMcSequence + process.BxToMuMuMcTables)
-    process.muonTable.variables.softMva = Var("softMvaValue()",float,doc="soft MVA ID score",precision=6)
-    return process
-
 def nanoAOD_customizeDileptonPlusX(process):
     process.load('Bmm5.NanoAOD.DileptonPlusX_cff')
     process.load('Bmm5.NanoAOD.UpdateSlimmedMuons_cff')
