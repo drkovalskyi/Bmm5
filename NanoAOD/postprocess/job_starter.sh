2178504e97bf8d124a670ab0f6b7a565dca9ef26
Dmytro Kovalskyi <dmytro@cern.ch>
2023-07-29 13:16:44 +0200

    python3 migration

--- NanoAOD/postprocess/job_starter.sh
+++ NanoAOD/postprocess/job_starter.sh
@@ -9,6 +9,6 @@ job_name=$2
 
 final_log=${job_name/%.job/.log}
 
-python job_starter.py $processor_name $job_name &> $tmp_log
+python3 job_starter.py $processor_name $job_name &> $tmp_log
 
 mv $tmp_log $final_log
